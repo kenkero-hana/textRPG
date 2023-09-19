1e9c0b533f18df62e6fdc692bf539c6c5d4e7cb0
kenkero-hana <watanabe.kenkanfu@gmail.com>
2023-09-19 12:03:50 +0900

    0919

--- Main.c
+++ Main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+typedef struct
+{
+    char name[50];
+    int health;
+    int Score;
+}player;
+
+
 int main(void){
+    player player;
     printf("ようこそ、TEXT・RPGの世界へ\n");
+    printf("プレイヤーの名前を入力してください\n");
+    scanf("%s", player.name);
+    printf("こんにちは、%s!ゲームを始めます。\n", player.name);
 
     return 0;
 }
